792e5082328c24a84511e80bb8c4b7ff0683743d
Dmytro-77 <108612579+Dmytro-77@users.noreply.github.com>
2022-09-05 18:41:40 +0300

    vas

--- js/menu.js
+++ js/menu.js
@@ -2,16 +2,21 @@
   const refs = {
     openMenuBtn: document.querySelector(".menu-open"),
     closeMenuBtn: document.querySelector(".menu-close"),
-      menu: document.querySelector(".menu"),
-      body: document.querySelector("body"),
+    menu: document.querySelector(".menu"),
+    body: document.querySelector("body"),
+    menuList: document.querySelector(".menu__list"),
   };
 
   refs.openMenuBtn.addEventListener("click", toggleMenu);
   refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  refs.menuList.addEventListener("click", removeMenu);
 
-  function toggleModal() {
-      refs.menu.classList.toggle("is-hidden");
-       refs.body.classList.toggle("no-scroll");
-      
+  function toggleMenu() {
+    refs.menu.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll");
+  }
+
+  function removeMenu() {
+    refs.menu.classList.add("is-hidden");
   }
 })();
